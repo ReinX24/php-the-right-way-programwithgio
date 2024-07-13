ffdb394c6ca01502866ad274f34771bab55924c1
Rein <reinX244@gmail.com>
2024-07-13 15:18:39 +0800

    Finished magic methods.

--- 2.12_magic_methods/src/app/Invoice.php
+++ 2.12_magic_methods/src/app/Invoice.php
@@ -1,14 +1,62 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
+
+use Stringable;
 
 class Invoice
 {
     protected array $data;
 
+    private float $amount;
+    private int $id = 1;
+    private string $accountNumber = "0123456789";
+
     public function __construct($name, $value)
     {
         $this->data[$name] = $value;
+    }
+
+    // var_dump method output
+    public function __debugInfo()
+    {
+        return [
+            "id" => $this->id,
+            // Showing only the last 4 characters of accountNumber
+            "accountNumber" => "****" . substr($this->accountNumber, -4)
+        ];
+    }
+
+    public function __invoke()
+    {
+        var_dump("invoked");
+    }
+
+    public function __toString(): string
+    {
+        return "hello";
+    }
+
+    protected function process(float $amount, string $description)
+    {
+        var_dump($amount, $description);
+    }
+
+    // Triggers when an object calls a function
+    public function __call(string $name, array $arguments)
+    {
+        // Checks if the method exists in the class
+        if (method_exists($this, $name)) {
+            call_user_func_array([$this, $name], $arguments);
+        }
+        // var_dump($name, $arguments);
+    }
+
+    public static function __callStatic($name, $arguments)
+    {
+        var_dump("static", $name, $arguments);
     }
 
     public function __get($name)
@@ -25,5 +73,18 @@ class Invoice
     {
         // Sets the key and value in the data array
         $this->data[$name] = $value;
+    }
+
+    // Checks if the key is in the data array
+    public function __isset($name)
+    {
+        var_dump("isset");
+        return array_key_exists($name, $this->data);
+    }
+
+    public function __unset($name)
+    {
+        var_dump("unset");
+        unset($this->data[$name]);
     }
 }
--- 2.12_magic_methods/src/public/index.php
+++ 2.12_magic_methods/src/public/index.php
@@ -6,10 +6,39 @@ require_once "../vendor/autoload.php";
 
 $invoice = new Invoice("amount", 15);
 
-$invoice->amount = 20;
-
-// echo $invoice->amount; // triggers the __get magic method
-
 echo "<pre>";
 var_dump($invoice);
 echo "</pre>";
+
+// var_dump(is_callable($invoice)); 
+
+// $invoice(); // calls the __invoke magic method
+
+// var_dump($invoice instanceof Stringable);
+
+// $invoice->amount = 20;
+
+// $invoice->process(15, "This is a description");
+// Invoice::process(1, 2, 3);
+
+// echo "<pre>";
+// var_dump($invoice->__isset("amount"));
+// echo "</pre>";
+
+// echo "<pre>";
+// var_dump($invoice);
+// echo "</pre>";
+
+// echo "<pre>";
+// var_dump($invoice->__unset("amount"));
+// echo "</pre>";
+
+// echo "<pre>";
+// var_dump($invoice);
+// echo "</pre>";
+
+// echo $invoice->amount; // triggers the __get magic method
+
+// echo "<pre>";
+// var_dump($invoice);
+// echo "</pre>";
